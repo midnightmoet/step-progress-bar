ad91a084ad8d3a83c731002e2cc54cb46ff23294
Lisa Miner <midnightmoet@gmail.com>
2023-08-03 22:29:13 -0400

    FUNCTIONING!!

---
+++ script.js
@@ -0,0 +1,64 @@
+const nextEl = document.getElementById("next");
+const prevEl = document.getElementById("prev");
+const progressEl = document.querySelector(".progress-bar-front");
+const stepsEl = document.querySelectorAll(".step");
+
+let currentChecked = 1;
+
+// NEXT BUTTON
+nextEl.addEventListener("click", () => {
+  currentChecked++;
+  if (currentChecked > stepsEl.length) {
+    currentChecked = stepsEl.length;
+  }
+  updateStepProgress();
+});
+
+
+// PREV BUTTON
+prevEl.addEventListener("click", () => {
+  currentChecked--;
+  if (currentChecked < 1) {
+    currentChecked = 1;
+  }
+  updateStepProgress();
+});
+
+
+// UPDATE PROGRESS
+function updateStepProgress() {
+  stepsEl.forEach((stepEl, idx) => {
+    if (idx < currentChecked) {
+      stepEl.classList.add("checked");
+      stepEl.innerHTML = `
+      <i class="fas fa-check"></i>
+      <small>${
+        idx === 0
+          ? "Start"
+          : idx === stepsEl.length - 1
+          ? "Final"
+          : "Step " + idx
+      }</small>
+      `;
+    } else {
+      stepEl.classList.remove("checked");
+      stepEl.innerHTML = `
+      <i class="fas fa-times"></i>
+      `;
+    }
+  });
+
+  const checkedNumber = document.querySelectorAll(".checked");
+
+  progressEl.style.width =
+    ((checkedNumber.length - 1) / (stepsEl.length - 1)) * 100 + "%";
+
+  if (currentChecked === 1) {
+    prevEl.disabled = true;
+  } else if (currentChecked === stepsEl.length) {
+    nextEl.disabled = true;
+  } else {
+    prevEl.disabled = false;
+    nextEl.disabled = false;
+  }
+}
